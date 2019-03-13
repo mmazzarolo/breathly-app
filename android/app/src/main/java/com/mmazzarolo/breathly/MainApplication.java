4cb7358f88e18b0ca5bea32031f46ca1fe71c678
fabriziogiordano <giordano.fabrizio@gmail.com>
2019-03-13 10:42:33 -0400

    Update to react-native-community/async-storage

--- android/app/src/main/java/com/mmazzarolo/breathly/MainApplication.java
+++ android/app/src/main/java/com/mmazzarolo/breathly/MainApplication.java
@@ -3,6 +3,7 @@ package com.mmazzarolo.breathly;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
           new NavigationBarColorPackage(),
           new SplashScreenReactPackage()
       );
