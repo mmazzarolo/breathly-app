685d747fbb365a4c0246b5451e31ef2fab3fc5ef
Matteo Mazzarolo <mazzarolomatteo@gmail.com>
2019-08-03 18:12:04 +0200

    Updated react-native to 0.60.4 (#9)

--- android/app/src/main/java/com/mmazzarolo/breathly/MainApplication.java
+++ android/app/src/main/java/com/mmazzarolo/breathly/MainApplication.java
@@ -1,15 +1,18 @@
 package com.mmazzarolo.breathly;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -22,11 +25,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new NavigationBarColorPackage(),
-          new SplashScreenReactPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new NavigationBarColorPackage());
+      return packages;
     }
 
     @Override
--- android/app/src/main/java/com/mmazzarolo/breathly/SplashActivity.java
+++ android/app/src/main/java/com/mmazzarolo/breathly/SplashActivity.java
@@ -2,7 +2,7 @@ package com.mmazzarolo.breathly;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Created by matteomazzarolo on 01/03/2019.
