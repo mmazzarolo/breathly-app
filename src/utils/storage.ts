1b0629ef5fc93110f2dca118fd6884524979f4bf
Matteo Mazzarolo <mazzarolomatteo@gmail.com>
2019-08-03 18:32:27 +0200

    Moved to react-native-async-storage (#10)
    * Updated react-native to 0.60.4
    
    * Moved to react-native-async-storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,4 @@
-import { AsyncStorage } from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
 
 export const restoreTechniqueId = async () => {
   const storedTechniqueId = await AsyncStorage.getItem("techniqueId");
