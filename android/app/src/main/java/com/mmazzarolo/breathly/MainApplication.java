1116badb43aae7d02fc4db5c2481c275902808b0
mmazzarolo <mazzarolomatteo@gmail.com>
2020-05-10 22:42:12 +0200

    Fix Android

--- android/app/src/main/java/com/mmazzarolo/breathly/MainApplication.java
+++ android/app/src/main/java/com/mmazzarolo/breathly/MainApplication.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactApplication;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
