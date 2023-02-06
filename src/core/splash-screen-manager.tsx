d8c5a4607136a68558a45c55786b39e17186686d
Matteo Mazzarolo <matteom@spotify.com>
2023-02-06 22:50:43 +0100

    Forced the splash-screen for at least 1.5 sec

--- src/core/splash-screen-manager.tsx
+++ src/core/splash-screen-manager.tsx
@@ -1,9 +1,11 @@
 import { Asset } from "expo-asset";
 import Constants from "expo-constants";
 import * as SplashScreen from "expo-splash-screen";
-import React, { PropsWithChildren, useCallback, useEffect, useMemo, useState } from "react";
+import ms from "ms";
+import React, { PropsWithChildren, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Animated, Easing, StyleSheet, View } from "react-native";
 import { useHomeScreenStatusStore } from "@breathly/screens/home-screen/home-screen";
+import { delay } from "@breathly/utils/delay";
 
 // Instruct SplashScreen not to hide yet, we want to do this manually
 SplashScreen.preventAutoHideAsync().catch(() => {
@@ -12,6 +14,8 @@ SplashScreen.preventAutoHideAsync().catch(() => {
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const splashImageAsset = require("../../assets/splash.png");
+
+const waitBeforeHide = ms("1.5 sec");
 
 export const SplashScreenManager: React.FC<PropsWithChildren> = ({ children }) => {
   const [isSplashReady, setSplashReady] = useState(false);
@@ -33,19 +37,30 @@ export const SplashScreenManager: React.FC<PropsWithChildren> = ({ children }) =
 };
 
 const AnimatedSplashScreen: React.FC<PropsWithChildren> = ({ children }) => {
+  const mountTime = useRef(Date.now()).current;
   const animation = useMemo(() => new Animated.Value(1), []);
   const [isAppReady, setAppReady] = useState(false);
   const { isHomeScreenReady } = useHomeScreenStatusStore();
   const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
 
+  const showApp = async () => {
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - mountTime;
+    const remainingTime = elapsedTime > waitBeforeHide ? 0 : waitBeforeHide - elapsedTime;
+    if (remainingTime > 0) {
+      await delay(remainingTime);
+    }
+    Animated.timing(animation, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+      easing: Easing.inOut(Easing.quad),
+    }).start(() => setAnimationComplete(true));
+  };
+
   useEffect(() => {
     if (isAppReady && isHomeScreenReady) {
-      Animated.timing(animation, {
-        toValue: 0,
-        duration: 200,
-        useNativeDriver: true,
-        easing: Easing.inOut(Easing.quad),
-      }).start(() => setAnimationComplete(true));
+      showApp();
     }
   }, [isAppReady, isHomeScreenReady]);
 
